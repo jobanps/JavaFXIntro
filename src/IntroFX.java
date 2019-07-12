f9b2d1f9d118ac40e1639da930a7a89dc1ca8835
macstudent <macstudent@10.10.150.63>
2019-07-12 10:55:17 -0400

    intial Commit

---
+++ src/IntroFX.java
@@ -0,0 +1,30 @@
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class IntroFX extends Application{
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		// Create UI Components
+		Button button = new Button();
+		button.setText("Click Here");
+		//Put UI Components inside a container
+		StackPane root = new StackPane();
+		root.getChildren().add(button);
+		// add the container to the scene
+		primaryStage.setScene(new Scene(root,300,200));
+		primaryStage.setTitle("My First FX Code");
+		//show the scece
+		primaryStage.show();
+		
+		
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+
+}
